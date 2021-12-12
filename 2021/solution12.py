35dbfa11c4b9cfa2a725549d853926567ded5fbf
Bjarke Moensted <bjarkemoensted@gmail.com>
2021-12-12 10:51:55 +0100

    More solutions

---
+++ 2021/solution12.py
@@ -0,0 +1,71 @@
+from collections import Counter
+import copy
+import networkx as nx
+
+with open("input12.txt") as f:
+    edges = [line.strip().split("-") for line in f]
+
+G = nx.Graph()
+for u, v in edges:
+    G.add_edge(u, v)
+
+
+def path_has_multiple_small_caves(path):
+    """Checks if a path contains multiple visits to any small cave."""
+    small = [s for s in path if s.islower()]
+    counts = Counter(small)
+    res = any(v > 1 for v in counts.values())
+    return res
+
+
+def traverse_caves(G, allow_two_small_cave_visits=False):
+    """Breadth-first traverses all possible paths in the caverns on the input graph.
+    Returns all such paths.
+    If allow_two_small_cave_visits, we allow a single small cave to be visited twice.
+    Otherwise, small caves are visited at most once.
+    """
+
+    # Holds the paths we're still iterating on
+    growing_paths = [['start']]
+    # Holds the paths that have reached the 'end' node
+    done_paths = []
+
+    while growing_paths:
+        # Extend all existing paths by one step
+        new_paths = []
+        for path in growing_paths:
+            # Examine all neighbors of the last node in the path
+            last_node = path[-1]
+            for node in G.neighbors(last_node):
+                # Always room for a small cave if we haven't been there yet
+                allow_small_cave = node not in path
+                # If we've been, but one double visit is allowed, check if we've already 'used' the double visit
+                if not allow_small_cave and allow_two_small_cave_visits:
+                    allow_small_cave = node != "start" and not path_has_multiple_small_caves(path)
+
+                # The new cave fits in path if it is large, or we have room for a small cave
+                node_fits_in_path = node.isupper() or allow_small_cave
+                if node_fits_in_path:
+                    new_path = copy.deepcopy(path) + [node]
+                    new_paths.append(new_path)
+                #
+            #
+
+        # Mark any new paths that reach 'end' as done. Otherwise, keep iterating on them.
+        growing_paths = []
+        for path in new_paths:
+            path_is_done = path[-1] == 'end'
+            if path_is_done:
+                done_paths.append(path)
+            else:
+                growing_paths.append(path)
+            #
+        #
+    return done_paths
+
+
+paths = traverse_caves(G)
+print(f"Solution to star1: {len(paths)}.")
+
+paths_with_double_visits = traverse_caves(G, allow_two_small_cave_visits=True)
+print(f"Solution to star2: {len(paths_with_double_visits)}.")
