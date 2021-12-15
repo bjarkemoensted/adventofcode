797e50bac0c6710eddf8ecd5079edfedf1ed1294
Bjarke Moensted <bjarkemoensted@gmail.com>
2021-12-15 08:38:30 +0100

    More solutions

--- 2021/solution14.py
+++ 2021/solution14.py
@@ -2,90 +2,91 @@ from collections import Counter, defaultdict
 
 
 def parse(s):
+    """Parses contents of input file into:
+    a) starting polymer, e.g. 'NNBC',
+    b) growing instructions, e.g. 'ab -> c' (which means insert c in between a's and b's),
+    which is equivalent to chaning every 'ab' pair into one 'ac' and one 'cb' pair.
+    Format of replacements dict is {'ab': {'ac': 1, 'cb': 1}}."""
     starting_polymer, instructions = s.split("\n\n")
     replacements = {}
     for line in instructions.split("\n"):
+        # Figure out what to replacethe old string with
         old, insertion = line.strip().split(' -> ')
-        replacement = "".join([old[0], insertion])
+        pair_a = old[0] + insertion  # Make the string like 'ac'
+        pair_b = insertion + old[1]  # Make the string like 'cb'
+        replacement = {}
+        for pair in (pair_a, pair_b):
+            replacement[pair] = replacement.get(pair, 0) + 1
         replacements[old] = replacement
 
     return starting_polymer, replacements
 
 
-def read():
+def read_input():
+    """Read and parse the input"""
     with open("input14.txt") as f:
         s = f.read()
     return parse(s)
 
-sample = """NNCB
 
-CH -> B
-HH -> N
-CB -> H
-NH -> C
-HB -> C
-HC -> B
-HN -> C
-NN -> C
-BH -> H
-NC -> B
-NB -> B
-BN -> B
-BB -> N
-BC -> B
-CC -> N
-CN -> C"""
-
-
-
-polymer, replacements = read()
-polymer, replacements = parse(sample)
-
-
-def grow_polymer_one_step(polym):
-    newstring = ""
-    for i in range(len(polym)-1):
-        substring = polym[i:i+2]
-        newstring += replacements[substring]
-
-    newstring += polym[-1]
-    return newstring
+polymer, replacements = read_input()
 
 
 def get_substrings(s):
+    """Counts the occurrences of all pairs (length 2 substrings) in input string.
+    For instance, 'NNBC' -> {'NN': 1, 'NB': 1, 'BC': 1}"""
     d = {}
-    for i in range(len(s) - 2):
-        substring = s[i:i+2]
+    for i in range(len(s) - 1):
+        substring = s[i:i + 2]
         d[substring] = d.get(substring, 0) + 1
-    secondlast = s[-2]
-    d[secondlast] = 1
-    last = s[-1]
-    return d, last
+    return d
 
 
 def grow_polymer_sneaky(s, n_iterations=10):
-    substring_counts, lastchar = get_substrings(s)
-    newcounts = defaultdict(lambda: 0)
-    for substring, count in substring_counts.items():
-        try:
-            new_substring = replacements[substring]
-        except KeyError:
-            new_key = substring+lastchar
-            new_substring = replacements[new_key]
-            newcounts[new_substring[1]] = 1
-        print(substring, new_substring)
-        newcounts[new_substring] += count
-    print(newcounts)
+    """Grows a polymer sneakily. In each iteration, each pair (length 2 substring) grows two new pairs,
+    according to the replacement data in the replacement dict.
+    After n iterations, the number of occurrences of each letter is counted."""
+
+    # Represent the polymer as all its pairs
+    pairs = get_substrings(s)
+    for _ in range(n_iterations):
+        new_pairs = defaultdict(lambda: 0)
+        # For each pair, grow its two descendants
+        for pair, count in pairs.items():
+            added_pairs = replacements[pair]
+            for added_pair, weight in added_pairs.items():
+                new_pairs[added_pair] += count * weight
+            #
+        pairs = new_pairs
+
+    # Count each letter in the polymer
+    counts = {}
+    for substring, count in pairs.items():
+        for char in substring:
+            counts[char] = counts.get(char, 0) + count
+        #
+
+    # The method double counts every letter except the first and last. Compensate for this
+    first, last = s[0], s[-1]
+    counts[first] += 1
+    counts[last] += 1
+    assert all(v % 2 == 0 for v in counts.values())
+    res = {k: v // 2 for k, v in counts.items()}
+
+    return res
 
 
-print(polymer)
-grow_polymer_sneaky(polymer)
-
-#for _ in range(10):
-#    polymer = grow_polymer_one_step(polymer)
-
-#counts = sorted(Counter(polymer).items(), key=lambda t: t[1])
-#diff = counts[-1][1] - counts[0][1]
-#print(f"Difference between most and least common element: {diff}.")
+def compute_maxmin_diff(d):
+    # Takes a dict of counts and computes the differences between the most and least common object.
+    ordered = sorted(d.values())
+    diff = ordered[-1] - ordered[0]
+    return diff
 
 
+poly_a = grow_polymer_sneaky(polymer, 10)
+diff_a = compute_maxmin_diff(poly_a)
+print(f"Solution to star 1: {diff_a}")
+
+poly_b = grow_polymer_sneaky(polymer, 40)
+diff_b = compute_maxmin_diff(poly_b)
+print(f"Solution to star 1: {diff_b}")
