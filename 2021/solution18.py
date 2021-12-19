d4387147cd8cc277b03d2328976d6062b486a757
Bjarke Moensted <bjarkemoensted@gmail.com>
2021-12-19 02:15:02 +0100

    Sloooowly getting there

--- 2021/solution18.py
+++ 2021/solution18.py
@@ -1,4 +1,5 @@
 from ast import literal_eval as LE
+from copy import deepcopy
 import math
 
 with open("input18.txt") as f:
@@ -13,144 +14,141 @@ def parse(s):
 nums = parse(raw)
 
 
-def add(a, b):
-    res = [a, b]
-    return res
-
-
-def access(arr, inds):
+def _access_nested(arr, inds):
     ptr = arr
     for i in inds:
         ptr = ptr[i]
     return ptr
 
 
-def set_(arr, inds, val):
+def _set_nested(arr, inds, val):
     ptr = arr
     for i in inds[:-1]:
         ptr = ptr[i]
     ptr[inds[-1]] = val
 
 
-def determine_starting_ind(arr):
-    res = [0]
-    while isinstance(access(arr, res), list):
-        res.append(0)
-    return res
+class Number:
+    def __init__(self, arr, verbose=True):
+        self.arr = arr
+        self.verbose = verbose
 
+    def print(self, *args, **kwargs):
+        if self.verbose:
+            print(*args, **kwargs)
 
-def iterate_inds(arr, start=None, right=True):
-    inds = start
-    if inds is None:
-        inds = determine_starting_ind(arr)
-    inds = [ind for ind in inds]
-    step = 1 if right else -1
-    offset = 0 if right else 1
+    def __getitem__(self, inds):
+        return _access_nested(self.arr, inds)
 
-    while inds:
-        inds[-1] += step
-        val = None
-        try:
-            if inds[-1] < 0:
-                raise IndexError
-            val = access(arr, inds)
-        except IndexError:
-            inds.pop()
-        if isinstance(val, list):
-            inds.append(offset)
+    def __setitem__(self, inds, value):
+        val = deepcopy(value)
+        _set_nested(self.arr, inds, val)
 
-        yield inds
+    def __str__(self):
+        return str(self.arr)
 
+    def __repr__(self):
+        return self.__str__()
 
-def find_first_number_ind(arr, start, right=True):
-    start = [val for val in start]
-    for inds in iterate_inds(arr, start, right):
-        val = access(arr, inds)
-        if isinstance(val, int):
-            return inds
+    def __add__(self, other):
+        arr = [self.arr, other.arr]
+        res = type(self)(arr=arr)
+        res.print(f"After addition: {res}.")
+        res.reduce()
+        return res
+
+    def ispair(self, inds):
+        return isinstance(self[inds], list) and all(isinstance(val, int) for val in self[inds])
+
+    def _it(self, inds=None):
+        if inds is None:
+            inds = []
+        inds = deepcopy(inds)
+        for i in range(len(self[inds])):
+            inds_current_level = deepcopy(inds) + [i]
+            yield inds_current_level
+            if isinstance(self[inds_current_level], list):
+                for subinds in self._it(inds=inds_current_level):
+                    yield subinds
+                #
+            #
         #
-    return None
 
+    def split(self, inds):
+        self.print(f"Splitting at: {inds}, value={self[inds]}.")
+        mid = self[inds] / 2
+        a, b = math.floor(mid), math.ceil(mid)
+        self[inds] = [a, b]
 
-def explode(arr, inds):
-    x = access(arr, inds)
-    assert isinstance(x, list)
-    startinds = inds
+    def try_split(self):
+        for inds in self._it():
+            if isinstance(self[inds], int) and self[inds] >= 10:
+                self.split(inds)
+                return True
+            #
+        return False
 
-    for num, right in zip(x, (False, True)):
-        nextinds = find_first_number_ind(arr, startinds, right=right)
-        if nextinds is None:
-            continue
-        current = access(arr, nextinds)
-        new_val = num + current
-        set_(arr, nextinds, new_val)
-    set_(arr, startinds, 0)
+    def _scan_for_number(self, inds, right=True):
+        """Scans for the next number. Right and up if rigth = true, otherwise left and up"""
+        inds = deepcopy(inds)
+        while inds:
+            if right:
+                inds[-1] += 1
+            else:
+                inds[-1] -= 1
+            falloff = inds[-1] < 0 or inds[-1] >= len(self[inds[:-1]])
+            # TODO problemet er her, vi skal ogsaa "ned" i lister mode hoejre, f.eks.
+            if falloff:
+                inds.pop()
+                continue
+            else:
+                if isinstance(self[inds], int):
+                    return inds
 
+    def explode(self, inds):
+        self.print(f"Exploding at: {inds}, value={self[inds]}.")
+        # Make sure we're exploding ints only
+        a, b = self[inds]
+        assert all(isinstance(val, int) for val in (a, b))
+        assert len(inds) == 4
 
-def split(arr, inds):
-    val = access(arr, inds)
-    assert isinstance(val, int)
-    x = val/2
-    a = math.floor(x)
-    b = math.ceil(x)
-    new_pair = [a, b]
-    set_(arr, inds, new_pair)
-
-
-def try_explode(arr):
-    for inds in iterate_inds(arr):
-        stuff = access(arr, inds)
-        if len(inds) >= 4 and isinstance(stuff, list) and all(isinstance(elem, int) for elem in stuff):
-            print(f"Exploding {arr} at location {inds} (value {access(arr, inds)})")
-            explode(arr, inds)
-            return True
+        # Replace the 'exploded' pair with a zero
+        self[inds] = 0
+        # Increment the nearest neighbors with a and b
+        for num, right in zip((a, b), (False, True)):
+            numinds = self._scan_for_number(inds, right)
+            if numinds is not None:
+                self[numinds] += num
+            #
         #
-    return False
+
+    def try_explode(self):
+        #self.print(f"Attempting explode")
+        for inds in self._it():
+            if self.ispair(inds) and len(inds) >= 4:
+                self.explode(inds)
+                return True
+            #
+        return False
+
+    def reduce(self):
+        done = False
+        while not done:
+            exploded = self.try_explode()
+            if exploded:
+                self.print(f"After explode: {self}")
+                continue
+            else:
+                split = self.try_split()
+                if split:
+                    self.print(f"After split: {self}.")
+                    continue
+                #
+            done = not exploded and not split
 
 
-def try_split(arr):
-    for inds in iterate_inds(arr):
-        val = access(arr, inds)
-        if isinstance(val, int) and val >= 10:
-            print(f"Splitting {arr} at location {inds} (value {access(arr, inds)})")
-            split(arr, inds)
-            return True
-    return False
+a = Number([[[[4,3],4],4],[7,[[8,4],9]]])
+b = Number([1,1])
 
-
-def reduce(arr):
-    done = False
-    while not done:
-        if try_explode(arr):
-            continue
-        elif try_split(arr):
-            continue
-        else:
-            done = True
-        #
-    return
-
-
-def compute_final_sum(numbers):
-    running = numbers[0]
-    reduce(running)
-    for number in numbers[1:]:
-        running = add(running, number)
-        reduce(running)
-    return running
-
-
-s = """[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]
-[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]
-[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]
-[[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]
-[7,[5,[[3,8],[1,4]]]]
-[[2,[2,2]],[8,[8,1]]]
-[2,9]
-[1,[[[9,3],9],[[9,0],[0,7]]]]
-[[[5,[7,4]],7],1]
-[[[[4,2],2],6],[8,7]]"""
-
-nums = parse(s)[:2]
-
-meh = compute_final_sum(nums)
+c = a + b
+print(c)
