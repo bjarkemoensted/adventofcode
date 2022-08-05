945234909ae77802fe9d85890d394682d746fad4
Bjarke Moensted <bjarkemoensted@gmail.com>
2022-08-05 19:41:07 +0200

    22/12

--- 2015/solution22.py
+++ 2015/solution22.py
@@ -1,5 +1,5 @@
+import abc
 from copy import deepcopy
-import json
 
 # Read in data
 with open("input22.txt") as f:
@@ -7,192 +7,303 @@ with open("input22.txt") as f:
 
 
 def parse(s):
-    res = {}
+    raw = {}
     for line in s.split("\n"):
         k, v = line.split(": ")
-        res[k] = int(v)
+        raw[k] = int(v)
+
+    mapper = {"Hit Points": "hit_points", "Damage": "damage"}
+    res = {mapper[k]: v for k, v in raw.items()}
 
     return res
 
 
+class Character:
+    def __init__(self, name, hit_points, mana=0, armor=0, damage=0):
+        self.__name__ = name
+        self.hit_points = hit_points
+        self.mana = mana
+        self.armor = armor
+        self.damage = damage
+
+    def __str__(self):
+        s = f"{self.__name__} with {self.hit_points} Hit points, {self.mana} mana, and {self.armor} armor."
+        return s
+
+
 boss_stats = parse(puzzle_input)
-player_stats = {'Hit Points': 50, "Mana": 500, "Armor": 0}
+player_stats = {'hit_points': 50, "mana": 500}
 
 
-costs = {
-    "Magic Missile": 53,
-    "Drain": 73,
-    "Shield": 113,
-    "Poison": 173,
-    "Recharge": 229
-}
-
-
-class InvalidAction(Exception):
+class InvalidActionException(Exception):
     pass
 
 
-class Game:
-    all_actions = ["Recharge", "Shield", "Poison", "Magic Missile", "Drain"]
+class Spell(metaclass=abc.ABCMeta):
+    _cost = None
 
-    def __init__(self, player, boss, verbose=False, hard=False):
-        self.player = deepcopy(player)
-        self.boss = deepcopy(boss)
-        self.effects = {
-            "Recharge": 0,
-            "Shield": 0,
-            "Poison": 0
-        }
+    def __init__(self, caster, target=None, verbose=False, register_mana_callback=None):
+        self.caster = caster
+        self.target = target
         self.verbose = verbose
-        self.mana_spent = 0
-        self.hard = hard
+        self.register_mana_callback = register_mana_callback
 
-    def __str__(self):
-        keys = ["boss", "player", "effects"]
-        d = {k: getattr(self, k) for k in keys}
-        s = json.dumps(d, sort_keys=True, indent=4)
-        return s
+    @property
+    def __name__(self):
+        return str(self.__class__.__name__)
 
-    def print(self, *args, **kwargs):
+    def _print(self, *args, **kwargs):
         if self.verbose:
             print(*args, **kwargs)
 
-    def _action_allowed(self, action):
-        if action in self.effects and self.effects[action] > 0:
-            return False
-        if costs[action] > self.player["Mana"]:
-            return False
-        return True
+    def pay_mana(self):
+        self.caster.mana -= self._cost
+        if self.register_mana_callback is not None:
+            self.register_mana_callback(self._cost)
 
-    def get_allowed_actions(self):
-        actions = sorted([action for action in self.all_actions if self._action_allowed(action)])
-        return actions
+    def check_castable(self):
+        return self._cost <= self.caster.mana
 
-    def apply_effects(self):
-        if self.effects["Shield"] > 0:
-            self.effects["Shield"] -= 1
-            if self.effects["Shield"] == 0:
-                self.player["Armor"] = 0
+    @abc.abstractmethod
+    def casting_effect(self):
+        pass
+
+    def cast(self):
+        if not self.check_castable():
+            raise InvalidActionException("Cannot cast spell")
+        self.pay_mana()
+        self.casting_effect()
+
+
+class Effect(Spell):
+    _turns_active = None
+    _timer = 0
+
+    def check_castable(self):
+        return self._timer == 0 and super().check_castable()
+
+    def casting_effect(self):
+        self._timer = self._turns_active
+        self.effect_start()
+
+    def effect_start(self):
+        pass
+
+    def effect_end(self):
+        pass
+
+    def apply_effect(self):
+        pass
+
+    def tick(self):
+        """Apply any ongoing effects from spell"""
+        if self._timer <= 0:
+            return
+
+        self.apply_effect()
+        self._timer -= 1
+        self._print(f"{self.__name__}'s timer is now {self._timer}.")
+        # Apply effect that occurs when the effect spell ends
+        if self._timer == 0:
+            self.effect_end()
+
+
+class MagicMissile(Spell):
+    _cost = 53
+
+    def casting_effect(self):
+        self.target.hit_points -= 4
+
+
+class Drain(Spell):
+    _cost = 73
+
+    def casting_effect(self):
+        self.target.hit_points -= 2
+        self.caster.hit_points += 2
+
+
+class Shield(Effect):
+    _cost = 113
+    _turns_active = 6
+    _boost = 7
+
+    def effect_start(self):
+        self.caster.armor += self._boost
+        self._print(f"{self.caster.__name__} armor boosted to {self.caster.armor}")
+
+    def effect_end(self):
+        self.caster.armor -= self._boost
+        self._print(f"{self.caster.__name__} armor reduced back to {self.caster.armor}")
+
+
+class Poison(Effect):
+    _cost = 173
+    _turns_active = 6
+    _damage = 3
+
+    def apply_effect(self):
+        self._print(f"{self.__name__} deals {self._damage} damage.")
+        self.target.hit_points -= self._damage
+
+
+class Recharge(Effect):
+    _cost = 229
+    _turns_active = 5
+    _boost = 101
+
+    def apply_effect(self):
+        self._print(f"{self.__name__} provides {self._boost} mana.")
+        self.caster.mana += self._boost
+
+
+spellbook = [MagicMissile, Drain, Shield, Poison, Recharge]
+
+
+class Game:
+    def __init__(self, player, boss, verbose=False):
+        self.player = deepcopy(player)
+        self.boss = deepcopy(boss)
+        self.verbose = verbose
+        self.history = []
+        self.total_mana_spent = 0
+        self.spells = {}
+        for constructor in spellbook:
+            spell = constructor(
+                caster=self.player,
+                target=self.boss,
+                verbose=verbose,
+                register_mana_callback=self.add_mana
+            )
+            self.spells[spell.__name__] = spell
+
+    def add_mana(self, mana):
+        self.total_mana_spent += mana
+
+    def _print(self, *args, **kwargs):
+        if self.verbose:
+            print(*args, **kwargs)
+        #
+
+    def get_available_spells(self):
+        """Returns a list of the spells which may be cast"""
+        return list(self.spells.keys())
+
+    def _print_status(self):
+        s = f"- Player has {self.player.hit_points} hit points, {self.player.armor} armor and {self.player.mana} mana."
+        s += f"\n- Boss has {self.boss.hit_points} hit points."
+        self._print(s)
+
+    def _tick_player(self, spell):
+        self._print(f"-- Player turn --")
+        self._print_status()
+
+        self._apply_effects()
+        if self.outcome:
+            return
+
+        self._print(f"Player casts {spell}.")
+        self.spells[spell].cast()
+        self._print()
+
+    def _tick_boss(self):
+        self._print(f"-- Boss turn --")
+        self._print_status()
+
+        self._apply_effects()
+        if self.outcome:
+            return
+
+        damage = max(1, self.boss.damage - self.player.armor)
+        self.player.hit_points -= damage
+        self._print(f"-Boss attacks for {damage} damage.")
+        self._print()
+
+    def _apply_effects(self):
+        for spell_name, spell in self.spells.items():
+            if isinstance(spell, Effect):
+                spell.tick()
             #
-        if self.effects["Recharge"] > 0:
-            self.player["Mana"] += 101
-            self.effects["Recharge"] -= 1
-        if self.effects["Poison"] > 0:
-            self.boss["Hit Points"] -= 3
-            self.effects["Poison"] -= 1
+        #
 
-    def cast_spell(self, spell):
-        if spell not in self.get_allowed_actions():
-            raise InvalidAction
+    @property
+    def outcome(self):
+        if self.player.hit_points <= 0:
+            return "loss"
+        elif self.boss.hit_points <= 0:
+            return "win"
+        else:
+            return None
 
-        self.player["Mana"] -= costs[spell]
-        if self.player["Mana"] < 0:
-            raise InvalidAction
-        self.mana_spent += costs[spell]
+    def tick(self, spell):
+        self.history.append(spell)
+        self._tick_player(spell)
+        if self.outcome:
+            self._print(f"*** {self.outcome.upper()} ***")
+            return self.outcome
 
-        if spell == "Magic Missile":
-            self.boss["Hit Points"] -= 4
-        elif spell == "Drain":
-            self.boss["Hit Points"] -= 2
-            self.player["Hit Points"] += 2
-        elif spell == "Shield":
-            self.player["Armor"] = 7
-            self.effects["Shield"] = 6
-        elif spell == "Poison":
-            self.effects["Poison"] = 6
-        elif spell == "Recharge":
-            self.effects["Recharge"] = 5
-
-    def tick(self, action):
-        # player turn
-        self.print("-- Player turn --")
-        self.print("- Player has {Hit Points} hit points, {Armor} armor, and {Mana} mana.".format(**self.player))
-        self.print(f"- Boss has {self.boss['Hit Points']} hit points.")
-        if self.hard:
-            self.player["Hit Points"] -= 1
-        if self.player["Hit Points"] <= 0:
-            return "Loss"
-        self.print(f"Player casts {action}.")
-        self.print()
-
-        self.apply_effects()
-        if self.boss["Hit Points"] <= 0:
-            self.print("Boom!")
-            return "Win"
-        self.cast_spell(action)
-
-        # boss turn
-        self.print("-- Boss turn --")
-        self.print("- Player has {Hit Points} hit points, {Armor} armor, and {Mana} mana.".format(**self.player))
-        self.print(f"- Boss has {self.boss['Hit Points']} hit points.")
-
-        self.apply_effects()
-        if self.boss["Hit Points"] <= 0:
-            self.print("Boom!")
-            return "Win"
-        damage = max(1, self.boss["Damage"] - self.player["Armor"])
-        self.player["Hit Points"] -= damage
-        if self.player["Hit Points"] <= 0:
-            return "Loss"
-
-        attackstring = str(max(1, self.boss["Damage"] - self.player["Armor"]))
-        if self.player["Armor"]:
-            attackstring = f'{self.boss["Damage"]} - {self.player["Armor"]} = ' + attackstring
-
-        self.print(f"Boss attacks for {attackstring} damage!")
-
-        self.print()
+        self._tick_boss()
+        if self.outcome:
+            self._print(f"*** {self.outcome.upper()} ***")
+            return self.outcome
 
 
-def bfs(initial_game):
-    lowest_spent = float("inf")
-    games = [deepcopy(initial_game)]
-    completed_games = []
-    done = False
+player = Character("Player", **player_stats)
+boss = Character("Boss", **boss_stats)
+
+
+def brute_force(starting_game: Game):
+    best = float('inf')
+    best_game = None
+    longest_string = 0
+
+    games = [deepcopy(starting_game)]
     n_its = 0
-
-    while not done:
-        # List of the games to 'grow' (applying all possible actions and noting the resulting new states)
-        new_states = []
-        for i in range(len(games)-1, -1, -1):
-            game = games[i]
-
-            allowed = game.get_allowed_actions()
-            # If no actions are possible, the game is through
-            if not allowed:
-                del games[i]
-                continue
-
-            # Try every possible action
-            for action in game.get_allowed_actions():
-                newgame = deepcopy(game)
-                outcome = newgame.tick(action)
+    while games:
+        next_iteration = []
+        for game in games:
+            for spell in game.get_available_spells():
+                new_game = deepcopy(game)
+                try:
+                    outcome = new_game.tick(spell=spell)
+                except InvalidActionException:
+                    continue
+                # If the game isn't over, and we haven't exceeded the best (lowest) amount of mana, keep iterating game
                 if outcome is None:
-                    if newgame.mana_spent < lowest_spent:
-                        new_states.append(newgame)
-                else:
-                    completed_games.append(newgame)
-                    if outcome == "Win":
-                        lowest_spent = min(lowest_spent, newgame.mana_spent)
+                    if new_game.total_mana_spent < best:
+                        next_iteration.append(new_game)
+                elif outcome == "win":
+                    if new_game.total_mana_spent < best:
+                        best = new_game.total_mana_spent
+                        best_game = new_game
+                    #
                 #
             #
         n_its += 1
-        games = new_states
-        done = len(games) == 0
-        msg = f"Ran {n_its} iterations. Grew {len(games)} games states. Got {len(games)} games total."
-        msg += f" Lowest boss health: {min([g.boss['Hit Points'] for g in games+completed_games])}."
-
+        games = next_iteration
+        msg = f"Completed {n_its} growth iterations. Iterating {len(games)} games in next cycle. Mana record: {best}."
+        longest_string = max(longest_string, len(msg))
+        msg += (longest_string - len(msg))*" "
         print(msg, end="\r")
     print()
 
-    return lowest_spent
+    return best_game
 
 
-game1 = Game(player=player_stats, boss=boss_stats)
-best = bfs(game1)
-print(f"Lowest amount of mana spent on winning game: {best}.")
+game1_initial = Game(player, boss)
+game1_final = brute_force(starting_game=game1_initial)
+print(f"Lowest possible mana expense for boss fight: {game1_final.total_mana_spent}.")
 
-game2 = Game(player=player_stats, boss=boss_stats, hard=True)
-best2 = bfs(game2)
-print(f"Lowest amount of mana spent on winning a hard game: {best2}.")
-# 1295 too high
+
+class HardGame(Game):
+    def _tick_player(self, spell):
+        self.player.hit_points -= 1
+        if self.outcome:
+            return
+        return super()._tick_player(spell)
+    #
+
+
+hard_game_initial = HardGame(player, boss)
+hard_game_final = brute_force(hard_game_initial)
+print(f"Lowest possible mana expense for hard boss fight: {hard_game_final.total_mana_spent}.")
