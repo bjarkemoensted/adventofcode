1076f7d39bb716d86a5bfc15cbd9337d081faa23
Bjarke Moensted <bjarkemoensted@gmail.com>
2023-12-27 12:00:49 +0100

    cleanup

--- 2023/solution25.py
+++ 2023/solution25.py
@@ -1,21 +1,6 @@
 import networkx as nx
 
 
-test = """jqt: rhn xhk nvd
-rsh: frs pzl lsr
-xhk: hfx
-cmg: qnr nvd lhk bvb
-rhn: xhk bvb hfx
-bvb: xhk hfx
-pzl: lsr hfx nvd
-qnr: nvd
-ntq: jqt hfx bvb xhk
-nvd: lhk
-lsr: lhk
-rzs: qnr cmg lsr rsh
-frs: qnr lhk lsr"""
-
-
 def read_input():
     with open("input25.txt") as f:
         puzzle_input = f.read()
