32da52127f5e1efbd265f5cf8f93ddd6a00976fa
Bjarke Moensted <bjarkemoensted@gmail.com>
2024-08-11 21:12:46 +0200

    Made a much faster solution for 2015 day 22

--- aoc/aoc_2015/solution22.py
+++ aoc/aoc_2015/solution22.py
@@ -1,5 +1,4 @@
-import abc
-from copy import deepcopy
+import heapq
 
 
 def parse(s):
@@ -8,300 +7,301 @@ def parse(s):
         k, v = line.split(": ")
         raw[k] = int(v)
 
-    mapper = {"Hit Points": "hit_points", "Damage": "damage"}
+    mapper = {"Hit Points": "boss_hp", "Damage": "boss_damage"}
     res = {mapper[k]: v for k, v in raw.items()}
 
     return res
 
 
-class Character:
-    def __init__(self, name, hit_points, mana=0, armor=0, damage=0):
-        self.__name__ = name
-        self.hit_points = hit_points
-        self.mana = mana
-        self.armor = armor
-        self.damage = damage
+class Queue:
+    """Queue thingy for the A* algorithm"""
+    def __init__(self):
+        self._items = []
+        self._set = set([])
 
-    def __str__(self):
-        s = f"{self.__name__} with {self.hit_points} Hit points, {self.mana} mana, and {self.armor} armor."
-        return s
+    def push(self, item, priority):
+        heapq.heappush(self._items, (priority, item))
+        self._set.add(item)
+
+    def pop(self):
+        _, item = heapq.heappop(self._items)
+        self._set.remove(item)
+        return item
+
+    def __bool__(self):
+        return bool(self._items)
+
+    def __len__(self):
+        return len(self._items)
+
+    def __contains__(self, item):
+        return item in self._set
+    #
 
 
-class InvalidActionException(Exception):
+class InvalidMoveError(Exception):
     pass
 
 
-class Spell(metaclass=abc.ABCMeta):
-    _cost = None
-
-    def __init__(self, caster, target=None, verbose=False, register_mana_callback=None):
-        self.caster = caster
-        self.target = target
-        self.verbose = verbose
-        self.register_mana_callback = register_mana_callback
-
-    @property
-    def __name__(self):
-        return str(self.__class__.__name__)
-
-    def _print(self, *args, **kwargs):
-        if self.verbose:
-            print(*args, **kwargs)
-
-    def pay_mana(self):
-        self.caster.mana -= self._cost
-        if self.register_mana_callback is not None:
-            self.register_mana_callback(self._cost)
-
-    def check_castable(self):
-        return self._cost <= self.caster.mana
-
-    @abc.abstractmethod
-    def casting_effect(self):
-        pass
-
-    def cast(self):
-        if not self.check_castable():
-            raise InvalidActionException("Cannot cast spell")
-        self.pay_mana()
-        self.casting_effect()
-
-
-class Effect(Spell):
-    _turns_active = None
-    _timer = 0
-
-    def check_castable(self):
-        return self._timer == 0 and super().check_castable()
-
-    def casting_effect(self):
-        self._timer = self._turns_active
-        self.effect_start()
-
-    def effect_start(self):
-        pass
-
-    def effect_end(self):
-        pass
-
-    def apply_effect(self):
-        pass
-
-    def tick(self):
-        """Apply any ongoing effects from spell"""
-        if self._timer <= 0:
-            return
-
-        self.apply_effect()
-        self._timer -= 1
-        self._print(f"{self.__name__}'s timer is now {self._timer}.")
-        # Apply effect that occurs when the effect spell ends
-        if self._timer == 0:
-            self.effect_end()
-
-
-class MagicMissile(Spell):
-    _cost = 53
-
-    def casting_effect(self):
-        self.target.hit_points -= 4
-
-
-class Drain(Spell):
-    _cost = 73
-
-    def casting_effect(self):
-        self.target.hit_points -= 2
-        self.caster.hit_points += 2
-
-
-class Shield(Effect):
-    _cost = 113
-    _turns_active = 6
-    _boost = 7
-
-    def effect_start(self):
-        self.caster.armor += self._boost
-        self._print(f"{self.caster.__name__} armor boosted to {self.caster.armor}")
-
-    def effect_end(self):
-        self.caster.armor -= self._boost
-        self._print(f"{self.caster.__name__} armor reduced back to {self.caster.armor}")
-
-
-class Poison(Effect):
-    _cost = 173
-    _turns_active = 6
-    _damage = 3
-
-    def apply_effect(self):
-        self._print(f"{self.__name__} deals {self._damage} damage.")
-        self.target.hit_points -= self._damage
-
-
-class Recharge(Effect):
-    _cost = 229
-    _turns_active = 5
-    _boost = 101
-
-    def apply_effect(self):
-        self._print(f"{self.__name__} provides {self._boost} mana.")
-        self.caster.mana += self._boost
-
-
-spellbook = [MagicMissile, Drain, Shield, Poison, Recharge]
-
-
 class Game:
-    def __init__(self, player, boss, verbose=False):
-        self.player = deepcopy(player)
-        self.boss = deepcopy(boss)
+    player_hp_ind = 0
+    player_mana_ind = 1
+    boss_hp_ind = 2
+    shield_timer_ind = 3
+    poison_timer_ind = 4
+    recharge_timer_ind = 5
+
+    shield_effect = 7
+    recharge_effect = 101
+
+    spells = ("magic_missile", "drain", "shield", "poison", "recharge")
+    spell_costs = dict(
+        magic_missile=53,
+        drain=73,
+        shield=113,
+        poison=173,
+        recharge=229
+    )
+
+    spell_damage = dict(
+        magic_missile=4,
+        drain=2,
+        poison=3
+    )
+
+    spell_duration = dict(
+            shield=6,
+            poison=6,
+            recharge=5
+        )
+
+    def __init__(
+            self,
+            boss_hp,
+            boss_damage,
+            player_hp=50,
+            player_mana=500,
+            hard_mode=False,
+            verbose=False
+        ):
+
+        self.boss_damage = boss_damage
+        self.boss_hp = boss_hp
+        self.player_hp = player_hp
+        self.player_mana = player_mana
+        self.hard_mode = hard_mode
         self.verbose = verbose
-        self.history = []
-        self.total_mana_spent = 0
-        self.spells = {}
-        for constructor in spellbook:
-            spell = constructor(
-                caster=self.player,
-                target=self.boss,
-                verbose=verbose,
-                register_mana_callback=self.add_mana
-            )
-            self.spells[spell.__name__] = spell
 
-    def add_mana(self, mana):
-        self.total_mana_spent += mana
-
-    def _print(self, *args, **kwargs):
+    def vprint(self, *args, **kwargs):
         if self.verbose:
             print(*args, **kwargs)
-        #
 
-    def get_available_spells(self):
-        """Returns a list of the spells which may be cast"""
-        return list(self.spells.keys())
+    def make_initial_state(self):
+        res = [0 for _ in range(6)]
+        res[self.player_hp_ind] = self.player_hp
+        res[self.player_mana_ind] = self.player_mana
+        res[self.boss_hp_ind] = self.boss_hp
+        res = tuple(res)
+        return res
 
-    def _print_status(self):
-        s = f"- Player has {self.player.hit_points} hit points, {self.player.armor} armor and {self.player.mana} mana."
-        s += f"\n- Boss has {self.boss.hit_points} hit points."
-        self._print(s)
+    def magic_missile(self, state):
+        state[self.boss_hp_ind] -= self.spell_damage["magic_missile"]
+        state[self.player_mana_ind] -= self.spell_costs["magic_missile"]
 
-    def _tick_player(self, spell):
-        self._print(f"-- Player turn --")
-        self._print_status()
+    def drain(self, state):
+        state[self.boss_hp_ind] -= self.spell_damage["drain"]
+        state[self.player_hp_ind] += self.spell_damage["drain"]
+        state[self.player_mana_ind] -= self.spell_costs["drain"]
 
-        self._apply_effects()
-        if self.outcome:
+    def poison(self, state):
+        if state[self.poison_timer_ind] != 0:
+            raise InvalidMoveError
+        state[self.player_mana_ind] -= self.spell_costs["poison"]
+        state[self.poison_timer_ind] = self.spell_duration["poison"]
+
+    def shield(self, state):
+        if state[self.shield_timer_ind] != 0:
+            raise InvalidMoveError
+        state[self.player_mana_ind] -= self.spell_costs["shield"]
+        state[self.shield_timer_ind] = self.spell_duration["shield"]
+
+    def recharge(self, state):
+        if state[self.recharge_timer_ind] != 0:
+            raise InvalidMoveError
+        state[self.player_mana_ind] -= self.spell_costs["recharge"]
+        state[self.recharge_timer_ind] = self.spell_duration["recharge"]
+
+    def game_over(self, state):
+        """Returns whether the game is over (player or boss is dead)"""
+        return any(state[ind] <= 0 for ind in (self.player_hp_ind, self.boss_hp_ind))
+
+    def won(self, state):
+        """Returns whether the game is won"""
+        return state[self.boss_hp_ind] <= 0 and state[self.player_hp_ind] > 0
+
+    def _apply_effects(self, state: list):
+        """Applies all effects currently in play, and decrements their timers"""
+
+        if self.game_over(state):
             return
 
-        self._print(f"Player casts {spell}.")
-        self.spells[spell].cast()
-        self._print()
-
-    def _tick_boss(self):
-        self._print(f"-- Boss turn --")
-        self._print_status()
-
-        self._apply_effects()
-        if self.outcome:
-            return
-
-        damage = max(1, self.boss.damage - self.player.armor)
-        self.player.hit_points -= damage
-        self._print(f"-Boss attacks for {damage} damage.")
-        self._print()
-
-    def _apply_effects(self):
-        for spell_name, spell in self.spells.items():
-            if isinstance(spell, Effect):
-                spell.tick()
-            #
+        if state[self.shield_timer_ind] > 0:
+            # Shield doesn't really do anything when the timer ticks. Armor effect on damage is handled during attacks
+            state[self.shield_timer_ind] -= 1
+            self.vprint(f"Shield provides {self.shield_effect} Armor. Timer now at {state[self.shield_timer_ind]}.")
+        if state[self.poison_timer_ind] > 0:
+            # Handle poison damage and timer
+            state[self.poison_timer_ind] -= 1
+            dmg = self.spell_damage["poison"]
+            state[self.boss_hp_ind] -= dmg
+            self.vprint(f"Poison deals {dmg} damage. timer is now {state[self.poison_timer_ind]}")
+        if state[self.recharge_timer_ind] > 0:
+            # Handle mana recharge effects
+            state[self.recharge_timer_ind] -= 1
+            state[self.player_mana_ind] += self.recharge_effect
+            msg = f"Recharge provides {self.recharge_effect} mana."
+            msg += f"Its timer is now {state[self.recharge_timer_ind]}"
+            self.vprint(f"Recharge gives {self.recharge_effect} mana. Timer now at {state[self.recharge_timer_ind]}.")
         #
 
-    @property
-    def outcome(self):
-        if self.player.hit_points <= 0:
-            return "loss"
-        elif self.boss.hit_points <= 0:
-            return "win"
-        else:
-            return None
+    def boss_turn(self, state: list):
+        """Runs the boss turn - attack taking any shield effect into consideration"""
 
-    def tick(self, spell):
-        self.history.append(spell)
-        self._tick_player(spell)
-        if self.outcome:
-            self._print(f"*** {self.outcome.upper()} ***")
-            return self.outcome
+        if self.game_over(state):
+            return
+        self.vprint(f"-- Boss turn --")
+        self.vprint(f"- State: {state}")
 
-        self._tick_boss()
-        if self.outcome:
-            self._print(f"*** {self.outcome.upper()} ***")
-            return self.outcome
+        dmg = self.boss_damage
+        if state[self.shield_timer_ind] > 0:
+            dmg = max(1, dmg - self.shield_effect)
+
+        state[self.player_hp_ind] -= dmg
+        self.vprint(f"Boss attacks for {dmg} damage.")
+        self.vprint(f"State after turn: {state}")
+
+    def player_turn(self, state: list, spell: str):
+        """Runs the player turn"""
+
+        if self.hard_mode:
+            state[self.player_hp_ind] -= 1
+
+        self.vprint(f"-- Player turn --")
+        self.vprint(f"- State: {state}")
+        self.vprint(f"Player casts {spell}")
+
+        # Cast the selected spell
+        fun = getattr(self, spell)
+        fun(state)
+
+        # Invalid move if we run out of mana
+        if state[self.player_mana_ind] < 0:
+            raise InvalidMoveError
+
+        self.vprint(f"State after turn: {state}")
+
+    def turn(self, state: tuple, spell: str | list):
+        """Run one or multiple spell casts"""
+
+        if isinstance(spell, list):
+            res = state
+            for spell_ in spell:
+                res = self.turn(state=res, spell=spell_)
+            return res
+
+        res = [val for val in state]
+        self.player_turn(state=res, spell=spell)
+        self._apply_effects(state=res)
+        self.vprint()
+        self.boss_turn(state=res)
+        self._apply_effects(state=res)
+        self.vprint()
+
+        res = tuple(res)
+        return res
+
+    def neighbors(self, state: tuple):
+        """Get all states it's possible to get to from the input state"""
+
+        res = []
+        for spell in self.spells:
+            cost = self.spell_costs[spell]
+            # Don't even try the ones we can't afford anyway
+            if cost > state[self.player_mana_ind]:
+                continue
+            try:
+                # Just try to cast any remaining spell, and handle exceptions if not possible
+                neighbor = self.turn(state=state, spell=spell)
+            except InvalidMoveError:
+                continue
+
+            res.append((spell, cost, neighbor))
+        return res
+
+    def heuristic(self, state: tuple):
+        """Could do somehthing smart here, but this runs plenty fast without an informative heuristic"""
+        return 0
 
 
-def brute_force(starting_game: Game):
-    best = float('inf')
-    best_game = None
-    longest_string = 0
+def play(game: Game):
+    """A*, wizarding version."""
 
-    games = [deepcopy(starting_game)]
-    n_its = 0
-    while games:
-        next_iteration = []
-        for game in games:
-            for spell in game.get_available_spells():
-                new_game = deepcopy(game)
-                try:
-                    outcome = new_game.tick(spell=spell)
-                except InvalidActionException:
-                    continue
-                # If the game isn't over, and we haven't exceeded the best (lowest) amount of mana, keep iterating game
-                if outcome is None:
-                    if new_game.total_mana_spent < best:
-                        next_iteration.append(new_game)
-                elif outcome == "win":
-                    if new_game.total_mana_spent < best:
-                        best = new_game.total_mana_spent
-                        best_game = new_game
-                    #
+    initial_state = game.make_initial_state()
+
+    d_g = dict()
+    d_g[initial_state] = 0
+
+    f_initial = game.heuristic(state=initial_state)
+
+    open_ = Queue()
+    open_.push(initial_state, priority=f_initial)
+
+    last_spell = dict()  # Keep track of the spells cast
+    camefrom = dict()
+
+    while open_:
+        current = open_.pop()
+        if game.won(current):
+            # Reconstruct the history of cast spells
+            link = current
+            rev = []
+            while link in camefrom:
+                spell = last_spell[link]
+                link = camefrom[link]
+                rev.append(spell)
+            res = rev[::-1]
+            return res
+        elif game.game_over(current):
+            continue  # Don't continue if the game is lost
+
+        for spell, cost, neighbor_state in game.neighbors(state=current):
+            g_tentative = d_g[current] + cost
+            improved = g_tentative < d_g.get(neighbor_state, float("inf"))
+            if improved:
+                last_spell[neighbor_state] = spell
+                camefrom[neighbor_state] = current
+                d_g[neighbor_state] = g_tentative
+                h = game.heuristic(state=neighbor_state)
+                f = g_tentative + h
+                open_.push(neighbor_state, priority=f)
                 #
             #
-        n_its += 1
-        games = next_iteration
-        msg = f"Completed {n_its} growth iterations. Iterating {len(games)} games in next cycle. Mana record: {best}."
-        longest_string = max(longest_string, len(msg))
-        msg += (longest_string - len(msg))*" "
-        print(msg, end="\r")
-    print()
-
-    return best_game
-
-
-class HardGame(Game):
-    def _tick_player(self, spell):
-        self.player.hit_points -= 1
-        if self.outcome:
-            return
-        return super()._tick_player(spell)
+        #
     #
 
 
 def solve(data: str):
     boss_stats = parse(data)
-    player_stats = {'hit_points': 50, "mana": 500}
+    game = Game(**boss_stats)
 
-    player = Character("Player", **player_stats)
-    boss = Character("Boss", **boss_stats)
-
-    game1_initial = Game(player, boss)
-    game1_final = brute_force(starting_game=game1_initial)
-    star1 = game1_final.total_mana_spent
+    path = play(game=game)
+    star1 = sum(game.spell_costs[spell] for spell in path)
     print(f"Solution to part 1: {star1}")
 
-    hard_game_initial = HardGame(player, boss)
-    hard_game_final = brute_force(hard_game_initial)
-    star2 = hard_game_final.total_mana_spent
+    hard_game = Game(**boss_stats, hard_mode=True)
+    path2 = play(hard_game)
+
+    star2 = sum(game.spell_costs[spell] for spell in path2)
     print(f"Solution to part 2: {star2}")
 
     return star1, star2
