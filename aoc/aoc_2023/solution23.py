7ed97e36ec302764b3044eeb266124793e911b43
bjarke <bjarkemoensted@gmail.com>
2025-11-22 18:13:28 +0100

    Got a numba version running pretty fast, even withou heuristic.

--- aoc/aoc_2023/solution23.py
+++ aoc/aoc_2023/solution23.py
@@ -3,16 +3,24 @@
 # *· . ·   `.  ·.•·    https://adventofcode.com/2023/day/23   ·   .*  ·.* ·`  ·.
 # ·`*    ··.`·.*      ·`·.*`·*   .·`.·  +·.      · `·.*    `   · ` ·    ·*.·.`+·
 
-from collections import Counter
 from dataclasses import dataclass
 from enum import Enum
-import itertools
+from heapq import heappop, heappush
+import numba
+from numba.typed import Dict, List
+from numba.types import DictType
 import numpy as np
 from numpy.typing import NDArray
+import time
 import typing as t
 
 
 coordtype: t.TypeAlias = tuple[int, int]
+edgetype: t.TypeAlias = tuple[coordtype, coordtype]
+
+nb_node = numba.types.uint64
+nb_inner = numba.types.DictType(nb_node, nb_node)
+nb_outer = numba.types.DictType(nb_node, nb_inner)
 
 
 class Symbols(str, Enum):
@@ -61,241 +69,224 @@ slopes = {
     left: Symbols.left,
 }
 
+_all_dirs: tuple[tuple[int, int], ...] = tuple(sorted(slopes.keys()))
+
 
 def parse(s: str) -> NDArray[np.str_]:
     M = np.array([list(line) for line in s.split("\n")])
     return M
 
 
-@dataclass(frozen=True)
-class Section:
-    points: tuple[coordtype, ...]
-    uphill: bool
-    
-    @property
-    def length(self) -> int:
-        return len(self.points)
-    #
-    
-    @property
-    def start(self) -> coordtype:
-        return self.points[0]
-    
-    @property
-    def end(self) -> coordtype:
-        return self.points[-1]
-    #
+def _dirs_and_neighbors(M: NDArray[np.str_], i: int, j: int) -> t.Iterator[tuple[tuple[int, int], coordtype]]:
+    """For a given site in the ASCII map, generates pairs of (direction, neighbor coordinates)."""
 
-
-def _determine_junctions(M: NDArray[np.str_]) -> set[coordtype]:
-    """Determine the 'junctions' - points where we enter/leave the maze, or where it's possible to turn"""
-    junctions: set[coordtype] = set()
-    for (i, j), char in np.ndenumerate(M):
-        if char == Symbols.wall:
-            continue
-        
-        if i == 0 or i == M.shape[0] - 1:
-            junctions.add((i, j))
-            continue
-        
-        n_neighbors = 0
-        for di, dj in slopes.keys():
-            xp = (i+di, j+dj)
-            if all(0 <= c < lim for c, lim in zip(xp, M.shape)):
-                n_neighbors += M[*xp] != Symbols.wall
-            #
-        if n_neighbors > 2:
-            assert M[i, j] == Symbols.free
-            junctions.add((i, j))
+    for dir_ in slopes.keys():
+        di, dj = dir_
+        x = (i + di, j + dj)
+        if all(0 <= c < lim for c, lim in zip(x, M.shape)) and M[*x] != Symbols.wall:
+            yield dir_, x
         #
-    return junctions
+    #
 
 
-def _iter_sections(M: NDArray[np.str_]) -> t.Iterator[Section]:
-    """Generates sections connecting junction points.
-    Runs a simple BFS from each junction, growing paths in each possible direction,
-    until other junction points are reached. Keeps track of whether the path
-    has gone uphill at any slopes"""
-    
-    junctions = _determine_junctions(M)
-    
-    for point in sorted(junctions):
-        # Initialize paths starting from this junction
-        visited: set[coordtype] = {point}
-        paths: list[tuple[tuple[coordtype, ...], bool]] = [((point,), False)]  # path so far, uphill
-        front: list[tuple[tuple[coordtype, ...], bool]] = []
-        
+def _iter_segments(M: NDArray[np.str_]) -> t.Iterator[tuple[tuple[coordtype, ...], bool]]:
+    """Given the ASCII map, generates every path segment, meaning all paths between non-trivial sites (with != 2 neighbors).
+    Generates for each such segment
+    points on path - tuple of the sites that make up the path segment,
+    uphill (bool) - indicates whether any point on the segment goes up a slope"""
+
+    sites = {x for x, char in np.ndenumerate(M) if char != Symbols.wall}
+    _adj_cache = {(i, j): tuple(_dirs_and_neighbors(M, i, j)) for (i, j) in sites}
+
+    branch_points = {x for x, adj_ in _adj_cache.items() if len(adj_) != 2}
+
+    for x in sorted(branch_points):
+        paths = [((x,), False)]
+        visited = {x}
+
         while paths:
-            for path in paths:
-                nodes, uphill = path
-                i, j = nodes[-1]
-                
-                # Try moving in all 4 directions
-                for dir_ in slopes.keys():
-                    di, dj = dir_
-                    neighbor = (i+di, j+dj)
-                    if not all(0 <= c < lim for c, lim in zip(neighbor, M.shape)):
-                        continue  # ignore steps that fall off
-                    if M[*neighbor] == Symbols.wall or neighbor in visited:
-                        continue  # ignore steps that hit a wall
-                    
-                    # Update the path nodes and uphill status
-                    uphill_here = M[i, j] in slopes.values() and slopes[dir_] != M[i, j]
-                    new_nodes = nodes + (neighbor,)
+            grow = paths
+            paths = []
+            for points, uphill in grow:
+                head = points[-1]
+                for step, neighbor in _adj_cache[head]:
+                    if neighbor in visited:
+                        continue
+                    visited.add(neighbor)
+
+                    char = M[*head]
+                    uphill_here = char != Symbols.free and slopes[step] != char
                     new_uphill = uphill or uphill_here
                     
-                    # Check if we reached another junction, or need to further grow the path
-                    if neighbor in junctions:
-                        section = Section(points=new_nodes, uphill=new_uphill)
-                        yield section
+                    new_points = points + (neighbor,)
+                    updated_path = (new_points, new_uphill)
+
+                    if neighbor in branch_points:
+                        yield updated_path
                     else:
-                        visited.add(neighbor)
-                        front.append((new_nodes, new_uphill))
+                        paths.append(updated_path)
+                    #
                 #
-            
-            paths = front
-            front = []
+            #
         #
     #
 
 
+# TODO fuck it just nuke this!!!!!
 class Graph:
     def __init__(self, M: NDArray[np.str_], allow_uphill=False) -> None:
         self.M = M.copy()
-        self.sections = tuple(section for section in _iter_sections(self.M) if allow_uphill or not section.uphill)
-        self._section_lookup = {(section.start, section.end): section for section in self.sections}
-        
-        self._distances: dict[coordtype, dict[coordtype, int]] = {
-            node: dict() for section in self.sections for node in (section.start, section.end)
-        }
-        
-        for section in self.sections:
-            self._distances[section.start][section.end] = section.length
-    
-    def nodes(self) -> tuple[coordtype, ...]:
-        return tuple(sorted(self._distances.keys()))
-    
-    def __getitem__(self, u: coordtype) -> dict[coordtype, int]:
-        return self._distances[u]
-    
-    def display_path(self, path: t.Sequence[coordtype]) -> None:
-        """Prints a visualization of the input path on the ASCII map, similar to on the AoC web page"""
-        M = self.M.copy()
-        for i in range(len(path)-1):
-            u, v = path[i:i+2]
-            section = self._section_lookup[(u, v)]
-            for p in section.points:
-                M[*p] = "O"
-            if i == 0:
-                M[*section.points[0]] = "S"
-        
-        print("\n".join(("".join(line) for line in M)))
+        self.d: dict[coordtype, dict[coordtype, int]] = dict()
+        self._points: dict[edgetype, tuple[coordtype, ...]] = dict()
 
+        for path, uphill in _iter_segments(M):
+            if allow_uphill or not uphill:
+                self.add_path(path)
+            #
+        #
 
-def _iter_layers(G: Graph, start: coordtype) -> t.Iterator[tuple[coordtype, ...]]:
-    """Iterates over nodes in 'layers', meaning the lowest number of path sections we must traverse
-    from start to the node. Iterates tuples of nodes in each layer."""
-    visited: set[coordtype] = set()
-    front = {start}
+    def add_node(self, u: coordtype) -> None:
+        if u not in self.d:
+            self.d[u] = dict()
+        #
     
-    while front:
-        yield tuple(sorted(front))
-        visited |= front
-        front = {neighbor for node in front for neighbor in G[node] if neighbor not in visited}
+    def add_edge(self, u: coordtype, v: coordtype, dist: int) -> None:
+        for node in (u, v):
+            self.add_node(node)
+        
+        self.d[u][v] = dist
+    
+    def add_path(self, path: tuple[coordtype, ...]) -> None:
+        dist = len(path) - 1
+        u, v = path[0], path[-1]
+        assert u != v
+        self.add_edge(u, v, dist)
+        self._points[(u, v)] = path
+
+    def nodes(self) -> list[coordtype]:
+        return sorted(self.d.keys())
+
+    def edges(self) -> list[edgetype]:
+        return sorted((u, v) for u, d_ in self.d.items() for v, _ in d_.items())
     #
 
 
-def _collapse_links(*links: tuple[coordtype, coordtype]) -> list[tuple[coordtype, coordtype]]:
-    """Takes a number of edges and 'collapses' them by collecting into the outermost points on each line segment.
-    For example, a line a -> b -> -> c -> ... -> z is collapsed into (a, z)."""
+@numba.njit
+def astar(graph: list[tuple[tuple[int, int], ...]], start: int, end: int):
+    nodes = list(range(len(graph)))
+    n_nodes = len(graph)
+
+    # Binary repr of each node has a single 1 in a distinct location. This allows efficient subset detection
+    assert n_nodes < 64 - 1
+    keys = [2**i for i in range(n_nodes)]
+    for i in range(len(keys)-1):
+        # Make sure we don't get overflow errors
+        assert keys[i] < keys[i+1]
+
+    # Absolute upper bound heuristic. This is terrible, just need something that's guaranteed to overshoot rn
+    upper_abs = 999
+    for e in graph:
+        for v, delta in e:
+            if delta != -1:
+                upper_abs += delta
+            #
+        #
+
+    dg = {(i, i): i for i in range(0)}
+
+    n_max = -1
+    initial_key = keys[start]
+
+    # map (head, key) for current longest dist
+    dg[(start, initial_key)] = 0
+
+    seed = (initial_key, start)
+    queue = [(-upper_abs, seed)]
+    nits = 0
+
+    record = -1
+
+    while queue and (n_max == -1 or nits <= n_max):
+        nits += 1
+
+        if nits % 1_000_000 == 0:
+            print(f"Queue size: {len(queue)} (nits={nits})")
+
+        h_old, (key, head) = heappop(queue)
+        h_old *= -1
+        dist = dg[(head, key)]
+
+        if head == end:
+            record = max(record, dist)
+            continue
+
+        # get neighbors
+        for neighbor, delta in graph[head]:
+            if neighbor == -1:
+                break
+            
+            if keys[neighbor] & key:
+                continue
+
+            new_dist = dist + delta
+            new_key = key + keys[neighbor]
+
+            improved = new_dist > dg.get((neighbor, new_key), -1)
+            # TODO actually compute something here!!!
+            new_upper_bound = upper_abs
+            f = new_dist + new_upper_bound
+
+            if improved and f > record:
+                dg[(neighbor, new_key)] = new_dist
+
+                
+                
+                new_state = (new_key, neighbor)
+                heappush(queue, (-f, new_state))
+            #
+        #
     
-    remaining = {u: v for u, v in links}
-    assert len(remaining) == len(links)
-    
-    res = []
-    
-    while remaining:
-        # Start with a random node, follow the links until we hit a dead end
-        u = next((k for k in remaining.keys()))
-        v = u
-        while v in remaining:
-            v = remaining.pop(v)
+    print(keys)
+    return record
+
+
+def longest_path(G: Graph, start: coordtype, end: coordtype) -> int:
+    # Represent nodes as consecutive ints from 0, and the graph as list of tuples of (neighbor, distance)
+    # This is more efficient with numba because of reasons!
+    nodes_ordered = sorted(G.nodes())
+    inv = {node: i for i, node in enumerate(nodes_ordered)}
+    stuff = [((-1, -1), (-1, -1), (-1, -1), (-1, -1)) for _ in nodes_ordered]
+
+    for u_i, u in enumerate(nodes_ordered):
+        adj = [(-1, -1) for _ in range(4)]
+        for ind, (v, dist) in enumerate(sorted(G.d[u].items())):
+            adj[ind] = (inv[v], dist)
         
-        # Found the two nodes where this segment points to the next layer. Add to results.
-        assert u != v
-        res.append((u, v))
-    
+        a, b, c, d = adj
+        stuff[u_i] = (a, b, c, d)
+
+
+    pre = time.time()
+    res = astar(stuff, inv[start], inv[end])
+
+    print(f"Delta: {time.time() - pre:.6f}")
+
     return res
 
 
-def longest_path(G: Graph, start: coordtype, end: coordtype) -> list[coordtype]:
-    
-    layers = list(_iter_layers(G, start))
-    assert len(layers[-1]) == 1 and layers[-1][0] == end
-    
-    # Represent each candidate path by its 'signature' tuple of tuples of destination nodes in edges to next layer,
-    # grouped by connected line segments. For example, ((p1,), (p2, p3,)) means one segment points 'down' to the next
-    # layer (to node p1), and another segment has 2 ends the point to the next layer at p2 and p3.
-    sigtype: t.TypeAlias = tuple[tuple[coordtype, ...], ...]
-    signature2best_length: dict[sigtype, int] = dict()
-    
-    
-    for i in range(len(layers)-1):
-        this_layer, next_layer = layers[i], layers[i+1]
-        dests = set(this_layer) | set(next_layer)
-        valid_edges = [(u, v) for u in this_layer for v in G[u] if v in dests]
-        combs = itertools.chain(*(itertools.combinations(valid_edges, n) for n in range(1, len(valid_edges)+1)))
-        
-        print(f"Layer: {i} - {this_layer}")
-        
-        for comb in combs:
-            # An odd number of path segments must pass through each layer, or we won't reach the final layer
-            links_forward = [(u, v) for u, v in comb if v in next_layer]
-            links_within_layer = [(u, v) for u, v in comb if v in this_layer]
-            
-            if len(links_forward) % 2 == 0:
-                continue
-            
-            # Count number of connections to/from each node. None must be > 1
-            from_, to_ = zip(*comb)
-            if any(len(links) != len(set(links)) for links in (from_, to_)):
-                continue
-            
-            internal_endpoints = _collapse_links(*links_within_layer)
-            for ep in internal_endpoints:
-                print(f"{ep=}", links_forward)
-            
-            if i == 0:
-                assert len(to_) == 1 and len(comb) == 1
-                edges_used = comb
-                signature = (to_,)
-                print(f"OMGOMG {to_=}, {from_=}, {comb=}")
-                print(f"SIG: {signature=}")
-            
-            #G.display_path(comb[0])
-            
-        
-        if i >= 1:
-            break  # !!!
-
-    raise NotImplementedError
-    
-
 def solve(data: str) -> tuple[int|str, ...]:
     M = parse(data)
-    
-    G = Graph(M, allow_uphill=True)
-    
+    G = Graph(M)
     start = min(G.nodes())
     end = max(G.nodes())
 
     
-    path1 = longest_path(G, start, end)
     # TODO solve puzzle
-    star1 = None
+    star1 = longest_path(G, start, end)
     print(f"Solution to part 1: {star1}")
 
-    star2 = None
+    G2 = Graph(M, allow_uphill=True)
+    star2 = longest_path(G2, start, end)
     print(f"Solution to part 2: {star2}")
 
     return star1, star2
@@ -304,7 +295,7 @@ def solve(data: str) -> tuple[int|str, ...]:
 def main() -> None:
     year, day = 2023, 23
     from aocd import get_data
-    #raw = get_data(year=year, day=day)
+    raw = get_data(year=year, day=day)
     solve(raw)
 
 
