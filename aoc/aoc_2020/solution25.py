51982cefde5e963c40b3e36de76bec7d8b95fb44
Bjarke Moensted <bjarkemoensted@gmail.com>
2025-11-30 12:19:39 +0100

    Started 2020 onboard

---
+++ aoc/aoc_2020/solution25.py
@@ -0,0 +1,33 @@
+# . ·`.` ·* · ··`  ·  `   ·*  .··   ·    · `+ . * ··.  +`*·    ·  ·.*`· *.·  ·`·
+# `  · *.` · . +·  .  *··`. ·*  + Combo Breaker  · *·`  .·`*·.      ·· *`+.·•.·`
+# *·· `  . · ·. *   *· https://adventofcode.com/2020/day/25  `· * *   ··  + ` .·
+# ·`.+· * ·`. ·  • *· ·`.• · `·  · * `· . +· ·* ·` .`+   .*· ·  +·    *  ·`.· ·*
+
+
+def parse(s: str) -> object:
+    res = s  # TODO parse input here
+    return res
+
+
+def solve(data: str) -> tuple[int|str, ...]:
+    parsed = parse(data)
+
+    # TODO solve puzzle
+    star1 = None
+    print(f"Solution to part 1: {star1}")
+
+    star2 = None
+    print(f"Solution to part 2: {star2}")
+
+    return star1, star2
+
+
+def main() -> None:
+    year, day = 2020, 25
+    from aocd import get_data
+    raw = get_data(year=year, day=day)
+    solve(raw)
+
+
+if __name__ == '__main__':
+    main()
